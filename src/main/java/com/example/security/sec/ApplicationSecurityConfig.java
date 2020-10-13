6a6d4fa81b3075fed9cff35f63f96f862e89ec89
laisk <laisk1@hotmail.com>
2020-10-13 17:57:53 +0300

    InMemoryUserDetailsManager creation

--- src/main/java/com/example/security/sec/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/sec/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.security.sec;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +23,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+      UserDetails kaisaMakkarainenUser  = User.builder()
+                .username("kaisaMakkarainen")
+                .password("password")
+                .roles("STUDENT")  //ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                kaisaMakkarainenUser
+
+        );
     }
 }
